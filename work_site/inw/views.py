e478f6be0935789175385fddfb67fb557aeee730
Patryk <swiety533@gmail.com>
2022-04-15 20:03:43 +0200

    table

--- work_site/inw/views.py
+++ work_site/inw/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def inw_view(request):
-    return render(request,'inw/inw.html')
+    import openpyxl
+    if request.method == 'POST':
+        excel_file = request.FILES['myfile']
+        wb_obj = openpyxl.load_workbook(excel_file)
+        sheet = wb_obj.get_sheet_by_name('Arkusz1')
+        dict = {col[0]: col[1:] for col in zip(*sheet.values)}
+        return render(request, 'inw/inw.html', {"dict": dict})
+    else:
+        return render(request,'inw/inw.html', {})
