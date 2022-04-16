28422eb5805ac8b52a927b2879882ff46e8c0c3b
Patryk <swiety533@gmail.com>
2022-04-16 10:44:07 +0200

    input table class

--- work_site/inw/views.py
+++ work_site/inw/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def inw_view(request):
-    import openpyxl
+    import pandas as pd
     if request.method == 'POST':
         excel_file = request.FILES['myfile']
-        wb_obj = openpyxl.load_workbook(excel_file)
-        sheet = wb_obj.get_sheet_by_name('Arkusz1')
-        dict = {col[0]: col[1:] for col in zip(*sheet.values)}
+        df = pd.read_excel(excel_file)
+        dict = df.to_dict('split')
+
         return render(request, 'inw/inw.html', {"dict": dict})
     else:
         return render(request,'inw/inw.html', {})
