4e5288ea16a1fc305032d831001f47ffa5276f45
ProSwiety <swiety533@gmail.com>
2022-05-20 00:08:29 +0200

    jsredirect delete

--- work_site/static/js/successful_download_redirect.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('link-with-redirect').addEventListener('click', function() {
-  setTimeout(function() {
-    // Should be triggered after download started
-    document.location.href='{% url "myapp:table" %}';
-  });
- }, false);
