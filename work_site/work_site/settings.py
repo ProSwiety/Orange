9df41e3366539278137749b7c3a7b79e10131786
ProSwiety <swiety533@gmail.com>
2023-03-25 15:03:49 +0100

    settings deploy test

--- work_site/work_site/settings.py
+++ work_site/work_site/settings.py
@@ -148,11 +148,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/var/www/proswiety/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / "static/")
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
